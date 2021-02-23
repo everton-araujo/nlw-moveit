e44c7ed1ec6f0ecd04846ec0fefce6b192665b02
Everton <everton.aaraujo@outlook.com>
2021-02-23 08:54:05 -0300

    project migrated to next.ts

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { ExperienceBar } from "../components/ExperienceBar";
+
+export default function Home() {
+  return (
+    <div className="container">
+
+
+      <ExperienceBar />
+    </div>
+  )
+}
